0ee67c846609a0e87f66788aa54aad7b171675e8
ghazaresan1 <itman888@yahoo.com>
2024-11-10 18:33:25 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,33 @@
 const CACHE_NAME = 'ghazaresan-portal-v1';
+const CACHE_ASSETS = [
+    '/notification/',
+    '/notification/index.html',
+    '/notification/app.js',
+    '/notification/manifest.json',
+    '/notification/icon.png',
+    '/notification/icon1.png'
+];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll([
-                  '/notification/',
-                '/notification/index.html',
-                '/notification/app.js',
-                '/notification/manifest.json',
-                '/notification/icon.png',
-                '/notification/icon1.png'
-            ]);
-        })
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(CACHE_ASSETS))
     );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
     );
 });
 
-self.addEventListener('push', (event) => {
+self.addEventListener('push', event => {
     const options = {
         body: event.data.text(),
         icon: '/notification/icon.png',
-        badge: '/notification/icon1.png'
+        badge: '/notification/icon1.png',
+        vibrate: [200, 100, 200]
     };
     
     event.waitUntil(
