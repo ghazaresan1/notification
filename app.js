0c64cd0fe99519e1b4e23f41d278b70a17766d6a
ghazaresan1 <itman888@yahoo.com>
2025-01-23 14:30:38 +0330

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const firebaseApp = firebase.initializeApp({
 
 // Register service worker with correct path
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/notification/service-worker.js')
+    navigator.serviceWorker.register('./service-worker.js')
         .then(registration => {
             console.log('Service Worker registered with scope:', registration.scope);
         })
