dda861be1c9411af78c795f01c99d9f79e83f726
ghazaresan1 <itman888@yahoo.com>
2025-01-27 09:46:43 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 async function requestNotificationPermission() {
     const permission = await Notification.requestPermission();
     if (permission === 'granted') {
-        registerServiceWorker();
+        // Generate a unique token for this device
+        const deviceToken = generateDeviceToken();
+        const registration = await registerServiceWorker();
+        
+        // Send the token to service worker
+        registration.active.postMessage({
+            fcmToken: deviceToken
+        });
     }
+}
+
+function generateDeviceToken() {
+    // Generate a unique identifier for this device
+    return 'device_' + Math.random().toString(36).substr(2, 9);
 }
 
 async function registerServiceWorker() {
@@ -10,6 +22,8 @@ async function registerServiceWorker() {
         scope: '/notification/'
     });
     console.log('Service Worker registered with scope:', registration.scope);
+    return registration;
 }
 
+// Start the notification setup
 requestNotificationPermission();
